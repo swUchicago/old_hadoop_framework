24e5c74c1548571bbcee75a95c9a90a78e7f334c
williamsentosa <williamsentosa.sentosa@gmail.com>
2018-02-13 21:20:48 +0700

    profilling

--- src/mapred/org/apache/hadoop/mapred/TaskTracker.java
+++ src/mapred/org/apache/hadoop/mapred/TaskTracker.java
@@ -1042,14 +1042,17 @@ public class TaskTracker
       int currentMaxException = jobClient.getCurrentMaxException();
 //      localMinSpaceStart = jobClient.getMinspacestart();
       localMinSpaceStart = minSpaceStart;
-      if (jobClient.getIsJobSubmitted()) {
-        System.out.println(jobClient.getJobStartTime() + "\t" + System.currentTimeMillis() + "\t" + currentMaxException + "\t" + localMinSpaceStart);
-      }
+//      if (jobClient.getIsJobSubmitted()) {
+//        System.out.println(jobClient.getJobStartTime() + "\t" + System.currentTimeMillis() + "\t" + currentMaxException + "\t" + localMinSpaceStart);
+//      }
     }
 
     if (askForNewTask) {
       checkLocalDirs(fConf.getLocalDirs());
       askForNewTask = enoughFreeSpace(localMinSpaceStart);
+      if (jobClient.getIsJobSubmitted()) {
+        System.out.println(askForNewTask);
+      }
     }
 
     //
@@ -1288,7 +1291,7 @@ public class TaskTracker
         df = new DF(new File(localDirs[i]), fConf);
         localDirsDf.put(localDirs[i], df);
       }
-
+      System.out.println("storage : " + df.getAvailable()/1000000);
       if (df.getAvailable() > minSpace)
         return true;
     }
